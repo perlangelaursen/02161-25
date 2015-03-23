d6392865260dcf26512114bf960aacbfd3e11b3b
Per Lange Laursen <perlangelaursen@gmail.com>
2015-03-23 16:33:06 +0100

    My tests added

---
+++ Softwarehuset/src/apptest/testExecutive.java
@@ -0,0 +1,14 @@
+package apptest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testExecutive {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
