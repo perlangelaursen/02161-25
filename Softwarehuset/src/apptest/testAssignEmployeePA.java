24515553795bfa025c82110ee45c289e6bd2a3dd
Per Lange Laursen <perlangelaursen@gmail.com>
2015-03-23 16:43:30 +0100

    Before created in testAssignEmployeePA and methods added in Employee

--- Softwarehuset/src/apptest/testAssignEmployeePA.java
+++ Softwarehuset/src/apptest/testAssignEmployeePA.java
@@ -2,13 +2,31 @@ package apptest;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class testAssignEmployeePA {
+import softwarehuset.Employee;
 
+public class testAssignEmployeePA {
+	@Before
+	public void setUp() {
+		Employee projectLeader = new Employee("Test", "RandD");
+		projectLeader.setProjectLeaderStatus(true);
+		Employee test1 = new Employee("Test2", "RandD");
+	}
+	
 	@Test
-	public void test() {
+	public void testAssignEmployeeProject() {
 		fail("Not yet implemented");
 	}
-
+	
+	@Test
+	public void testAssignEmployeeActivity() {
+		
+	}
+	
+	@Test
+	public void testEmployeeNotAvailable() {
+		
+	}
 }
--- Softwarehuset/src/softwarehuset/Employee.java
+++ Softwarehuset/src/softwarehuset/Employee.java
@@ -1,5 +1,18 @@
 package softwarehuset;
 
 public class Employee {
+	private String id;
+	private String department;
+	private boolean isProjectLeader = false;
+	
+	public Employee(String id, String department) {
+		// TODO Auto-generated constructor stub
+		this.id = id;
+		this.department = department;
+	}
+	
+	public void setProjectLeaderStatus(boolean status) {
+		this.isProjectLeader = status;
+	}
 
 }
