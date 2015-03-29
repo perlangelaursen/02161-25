169546409854e7a27bdddeaf8e0169997b12038f
Per Lange Laursen <perlangelaursen@gmail.com>
2015-03-29 14:55:38 +0200

    Relieve employee test created

---
+++ Softwarehuset/test/apptest/testRelieveEmployee.java
@@ -0,0 +1,56 @@
+package apptest;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import softwarehuset.Address;
+import softwarehuset.Company;
+import softwarehuset.Employee;
+import softwarehuset.Executive;
+import softwarehuset.OperationNotAllowedException;
+
+public class testRelieveEmployee {
+	public Company company;
+	public Employee projectLeader, test1;
+	
+	@Before
+	public void setUp() throws OperationNotAllowedException {
+		// Create company and executive
+		Address address = new Address("City", "Street");
+		company = new Company("Company", address);
+		Executive executive = new Executive("Name", "Department1", company, "password");
+		
+		// Log in as executive
+		assertFalse(company.executiveIsLoggedIn());
+		company.executiveLogin("password");
+		assertTrue(company.executiveIsLoggedIn());
+				
+		GregorianCalendar start = new GregorianCalendar();
+		GregorianCalendar end = new GregorianCalendar();
+		start.set(2015, Calendar.JANUARY, 23);
+		end.set(2015, Calendar.FEBRUARY, 23);
+		company.createProject("Project01", start, end);
+		company.createProject("Project02");
+				
+		projectLeader = new Employee("Test", "RandD");
+				
+		executive.assignProjectLeader(projectLeader,company.getSpecificProject(0));
+		test1 = new Employee("Test2", "RandD");
+		projectLeader.assignEmployeeProject(test1, company.getSpecificProject(0));
+	}
+	
+	@Test
+	public void testRelieveEmployee() {
+		assertEquals(1, company.getSpecificProject(0).getEmployees().size());
+		
+		projectLeader.relieveEmployeeProject(test1, company.getSpecificProject(0));
+		
+		assertEquals(0, company.getSpecificProject(0).getEmployees().size());
+	}
+
+}
