d1e755b845291457e81376fd3c44379d8fdacfb2
Per Lange Laursen <perlangelaursen@gmail.com>
2015-05-04 12:48:21 +0200

    UI added

---
+++ Softwarehuset/src/cmdinterface/WRTcmdinterface.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cmdinterface;
+
+/**
+ * @author perlangelaursen
+ *
+ */
+public class WRTcmdinterface {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
