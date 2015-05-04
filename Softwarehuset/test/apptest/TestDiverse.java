bea8126f53c30ac2eed23eb4b95fdf89a6318e92
anna0207 <annaoelgaardnielsen@hotmail.com>
2015-05-04 16:38:19 +0200

    temporary commit

--- Softwarehuset/test/apptest/TestDiverse.java
+++ Softwarehuset/test/apptest/TestDiverse.java
@@ -13,18 +13,35 @@ import static org.junit.Assert.*;
 public class TestDiverse {
 	
 	private Company company;
-	private Employee em2, em3;
+	private Executive executive;
+	private Employee em2, em3, pro;
+	private Project p;
+	private GregorianCalendar d1, d2, d3, d4;
 	
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
 		Address add = new Address("Kagevej 2", "Hundige");
 		company = new Company("Softwarehuset", add);
-		Executive executive = new Executive("Name", "Department1", company, "password");
+		executive = new Executive("Name", "Department1", company, "password");
 		
 		// Log in as executive
 		assertFalse(company.executiveIsLoggedIn());
 		company.executiveLogin("password");
 		assertTrue(company.executiveIsLoggedIn());
+		
+		p = company.createProject("project1");
+		pro = company.createEmployee("PROJ", "password", "department");
+		executive.assignProjectLeader(pro, p);
+		
+		d1 = new GregorianCalendar();
+		d2 = new GregorianCalendar();
+		d3 = new GregorianCalendar();
+		d4 = new GregorianCalendar();
+
+		d1.set(2015, 1, 1);
+		d2.set(2015, 2, 1);
+		d3.set(2015, 3, 1);
+		d4.set(2015, 4, 1);
 		
 	}
 	
@@ -64,13 +81,28 @@ public class TestDiverse {
 	
 	@Test
 	public void testGetEmployee() throws Exception {
-		
 		em3 = company.createEmployee("LAVT", "password", "department");
 		
 		assertEquals(company.getEmployee("LAVT"), em3);
 		assertEquals(company.getEmployee("HAVD"), em2);
 		assertNull(company.getEmployee("MIST"));
 		
+		
+	}
+	
+	@Test
+	public void testAddEmployeeToActivity() throws Exception {
+		company.employeeLogin(pro.getID(), pro.getPassword());
+		assertEquals(company.getLoggedInEmployee(), pro);
+		
+		pro.createActivity(p, "activity1", d1, d2);
+		pro.createActivity(p, "activity2", d3, d4);
+		
+		pro.assignEmployeeActivity(em2, p.getSpecificActivityByName(p.getID() + "-activity1"));
+		p.addEmployeeToActivity(em2, p.getSpecificActivityByName(p.getID() + "-activity1"));
+		assertTrue(p.getSpecificActivityByName(p.getID() + "-activity1").getEmployees().contains(em2));
+		p.addEmployeeToActivity(em3, p.getSpecificActivityByName(p.getID() + "-activity2"));
+		assertTrue(p.getSpecificActivityByName(p.getID() + "-activity2").getEmployees().contains(em3));
 		
 	}
 	
